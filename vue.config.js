376fdcba17163c00eed056786bfce52fbd647044
Sean Rice <seanriceaz@users.noreply.github.com>
2020-08-01 00:52:12 -0700

    Remove publicPath - it was causing issues in vue (#11)

--- vue.config.js
+++ vue.config.js
@@ -7,7 +7,6 @@ const plugins = process.env.NODE_ENV === 'production' ? [new TerserPlugin({
 })] : [];
 
 module.exports = {
-    publicPath: 'sleuth-sketch',
     outputDir: path.resolve(__dirname, "./docs"),
 
     lintOnSave: true,
